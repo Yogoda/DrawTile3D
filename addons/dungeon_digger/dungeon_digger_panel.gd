b5099bb3089b47c2d2f513e7caf110488d601184
Yogoda <joelfivat@hotmail.com>
2023-04-18 22:59:18 +0200

    add tile2d palette

--- addons/dungeon_digger/dungeon_digger_panel.gd
+++ addons/dungeon_digger/dungeon_digger_panel.gd
@@ -6,6 +6,28 @@ var draw_mode = DRAW_MODE.TILE3D
 
 var selected_color:Color = Color.CORNFLOWER_BLUE
 
+func _ready():
+	
+	build_tile_2d_list()
+	
+func build_tile_2d_list():
+	
+	var tile_list:ItemList = %Tile2DList
+	tile_list.clear()
+	
+	var image:ImageTexture = preload("res://atlas.image_texture.res")
+	
+	var icon:Texture2D = image
+	
+	for x in range(16):
+		
+		for y in range(16):
+			
+			var index = tile_list.add_item("", icon)
+			
+			tile_list.set_item_icon_region(index, Rect2(x*16, y*16, 16, 16))
+
+
 func set_selected_color(p_color:Color):
 	
 	selected_color = p_color
